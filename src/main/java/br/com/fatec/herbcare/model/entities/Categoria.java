1e643b8b05dce5a3c9e19fac9fb18dd5ef4f04f4
Amanda Muniz <dev.amandacsm@gmail.com>
2022-05-25 20:10:34 -0300

    Entidade Categoria

---
+++ src/main/java/br/com/fatec/herbcare/model/entities/Categoria.java
@@ -0,0 +1,27 @@
+package br.com.fatec.herbcare.model.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_categoria", schema = "db_herbcare_dev")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cd_categoria", nullable = false)
+    private Integer id;
+
+    @Column(name = "nm_categoria", nullable = false, unique = true, length = 45)
+    private String nome;
+
+    @Column(name = "ds_categoria", columnDefinition = "TINYTEXT")
+    private String descricao;
+
+}
