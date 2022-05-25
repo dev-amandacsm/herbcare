43233b6e03bc8646d5f9455080e57cf349c6bf23
Amanda Muniz <dev.amandacsm@gmail.com>
2022-05-24 23:04:29 -0300

    Projeto criado

---
+++ src/main/java/br/com/fatec/herbcare/HerbcareApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.herbcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fatec/herbcare/HerbcareApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fatec.herbcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
